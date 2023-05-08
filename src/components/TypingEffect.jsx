9b3ed0a667e33cad97340dc9f12e95d696ccc78a
filkovskyy <avfilkovsky@gmail.com>
2023-05-08 16:41:10 +0100

    fixed TypingEffect.jsx

--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 export default function TypingEffect(props) {
-  const [text, setText] = useState('');
-  const [blockPosition, setBlockPosition] = useState(0);
-  const { message } = props;
+    const [text, setText] = useState('')
+    const [blockPosition, setBlockPosition] = useState(0)
+    const { message } = props
 
-  useEffect(() => {
-    let i = 0;
-    const interval = setInterval(() => {
-      if (text.length >= message.length) {
-        clearInterval(interval);
-        return;
-      }
-      setText((prevText) => prevText + message.charAt(text.length));
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (text.length >= message.length) {
+                clearInterval(interval)
+                return
+            }
+            setText((prevText) => prevText + message.charAt(text.length))
+            setBlockPosition((text.length + 1) * 10)
+        }, 40)
 
-      setBlockPosition((text.length + 1) * 10);
+        return () => clearInterval(interval)
+    }, [message, text])
 
-      i++;
-    }, 40);
-
-    return () => clearInterval(interval);
-  }, [message, text]);
-
-  return <p style={{ left: `${blockPosition}px` }}>{text}</p>;
+    return <p style={{ left: `${blockPosition}px` }}>{text}</p>
 }
